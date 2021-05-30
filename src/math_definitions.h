324613a4680f7b2da668d630a96c66b8bfc0c84d
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-05-30 17:05:09 +0530

    Finish week-1 tasks

--- src/math_definitions.h
+++ src/math_definitions.h
@@ -3,14 +3,21 @@
 struct Vec
 {
     // TODO: What variables do we need to uniquely define a vector in 3D?
+    float x;
+    float y;
+    float z;
 };
 
 struct Ray
 {
     // TODO: What variables do we need to uniquely define a ray in 3D?
+    Vec origin;
+    Vec dir;
 };
 
 struct Sphere
 {
     // TODO: What variables do we need to uniquely define a sphere in 3D?
+    Vec origin;
+    float radius;
 };
